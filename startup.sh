a6488561bdcf831f5bfadf791dc4485d75ab1d37
gianluca mangiapelo <gianluca.mangiapelo@deltatre.com>
2021-02-07 14:40:30 +0100

    Update junit report

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 service influxdb start
 service grafana-server start
 
-k6 run -o influxdb=http://localhost:8086/load $1 | k6-to-junit junit.xml
+k6 run -o influxdb=http://localhost:8086/load $1 | k6-to-junit junit-$1.xml
 
 while true;
 do
